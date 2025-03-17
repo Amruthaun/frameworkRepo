0bbab8278c5f110bbba6aeef4d989ed79b867d14
Admin <Admin@DESKTOP-I3EH5DN.mshome.net>
2025-03-17 17:16:27 +0530

    first push

---
+++ SeleniumProject/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,17 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("exeute the create org Test");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modify org test");
+	}
+
+
+}
